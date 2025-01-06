273509c44ce5577857e8fff984cd73c51f6d2b33
Yuwang Cai <mrcaidev@gmail.com>
2025-01-06 21:30:32 +0800

    feat: sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import unocss from "unocss/astro";
 
 export default defineConfig({
   site: "https://mrcai.dev",
-  integrations: [unocss({ injectReset: true })],
+  integrations: [unocss({ injectReset: true }), sitemap()],
   redirects: {
     "/blog": "/blog/1",
   },
